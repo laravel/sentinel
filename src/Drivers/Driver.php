f9de903560fc2b024efd31b35923c47e966b5a68
crynobone <172966+crynobone@users.noreply.github.com>
2026-03-23 23:39:10 +0000

    Fix code styling

--- src/Drivers/Driver.php
+++ src/Drivers/Driver.php
@@ -13,7 +13,7 @@ abstract class Driver
     /**
      * Construct a new driver.
      *
-     * @param  \Closure(): \Illuminate\Contracts\Foundation\Application  $applicationResolver
+     * @param  Closure(): Application  $applicationResolver
      */
     public function __construct(protected Closure $applicationResolver)
     {
@@ -28,7 +28,7 @@ abstract class Driver
     /**
      * Authorize access for the request or throw an exception.
      *
-     * @throws \Illuminate\Auth\Access\AuthorizationException
+     * @throws AuthorizationException
      */
     public function authorizeOrFail(Request $request): void
     {
